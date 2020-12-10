ab76f3fb548b2223f8e10a42bcd0095a5d5da3d3
GitHub Action <action@github.com>
2020-12-10 23:28:15 +0100

    Add shebang

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 printf "\n=============================UPDATING CV=============================\n"
 
 make all PYTHON="python3" READ_GITHUB_TOKEN="$1" URL_GITHUB="$2" URL_STACKOVERFLOW="$3"
@@ -14,4 +16,5 @@ GITHUB_ACTOR=$4
 GITHUB_REPOSITORY=$5
 GITHUB_TOKEN=$6
 REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-git push "${REMOTE_REPO}"
+echo $REMOTE_REPO
+git push "${REMOTE_REPO}" -u origin master
